56faa55bdaec5c30855643053b8ba8711f575ffa
정준영 <jeongjy@mrblue.com>
2025-03-27 09:12:17 +0900

    feat: PessimisticLock을 이용한 동시성 제어

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -18,7 +18,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 class StockServiceTest {
 
     @Autowired
-    private StockService stockService;
+//    private StockService stockService;
+    private PessimisticLockStockService stockService;
 
     @Autowired
     private StockRepository stockRepository;
